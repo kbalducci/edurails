b7ae65640a430eeb34f32e38a85b88689217ec55
Kate <kbalducci@hotmail.com>
2015-02-04 11:56:53 -0600

    updated permits and belongs and has

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :companies
+
+  resources :courses
+
+  resources :locations
+
+  resources :employees
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
